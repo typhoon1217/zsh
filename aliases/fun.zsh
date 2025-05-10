eebe2e84c6a4c77329e5b4ba4b90eb8ad97a918f
typhoon <jungwooshin.97@gmail.com>
2025-05-10 16:15:53 +0900

    feat:fun xclip to wlcopy

--- aliases/fun.zsh
+++ aliases/fun.zsh
@@ -1,4 +1,4 @@
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed '\''s/^\s*[0-9]+\s*//;s/[;&|]*$//'\'')"'
 alias weather='curl wttr.in'
-alias shrug='echo "¯\_(ツ)_/¯" | xclip -selection clipboard'
-alias tableflip='echo "(╯°□°）╯︵ ┻━┻" | xclip -selection clipboard'
+alias shrug='echo "¯\_(ツ)_/¯" | wl-copy && echo "¯\_(ツ)_/¯"'
+alias tableflip='echo "(╯°□°）╯︵ ┻━┻" | wl-copy && echo "(╯°□°）╯︵ ┻━┻"'
