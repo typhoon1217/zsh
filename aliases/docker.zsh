e75a838463e331e47feeca7149035340167c85bc
typhoon <jungwooshin.97@gmail.com>
2025-06-04 22:25:39 +0900

    feat:docker compose up -d

--- aliases/docker.zsh
+++ aliases/docker.zsh
@@ -22,6 +22,7 @@ alias dnw='docker network ls'
 #docker compose
 alias dc='docker compose'
 alias dcu='docker compose up'
+alias dcud='docker compose up -d'
 alias dcd='docker compose down'
 alias dcr='docker compose run --rm'
 alias dce='docker compose exec'
